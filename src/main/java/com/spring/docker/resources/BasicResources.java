5003c9254c715e3d852f8bd2c7ffac8a633e1284
hemendrta <hemendra.singh.c@outlook.com>
2021-03-01 06:12:33 -0800

    creation of dockerfile, containerizing the app, running the image

---
+++ src/main/java/com/spring/docker/resources/BasicResources.java
@@ -0,0 +1,20 @@
+package com.spring.docker.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/docker")
+public class BasicResources {
+
+    @GetMapping("/welcome")
+    public String getDemoResource(){
+
+        return "This is demo url";
+
+    }
+
+}
+
+
